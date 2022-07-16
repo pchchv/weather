1970a3a84f8573669e5bdda3f2105771b1af5f37
pchchv <ipchchv@gmail.com>
2022-07-16 14:04:05 +0300

    Implement ping method

--- server.go
+++ server.go
@@ -5,7 +5,16 @@ import (
 	"net/http"
 )
 
+func ping(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	_, err := w.Write(getJSON("", "Random Value Service. Version 0.2"))
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
 func server() {
 	log.Println("Server started!")
+	http.HandleFunc("/ping", ping)
 	log.Fatal(http.ListenAndServe("localhost:8080", nil))
 }
