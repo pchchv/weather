935cca5cf6aae22db1955bb683d78efdb92f7b8b
pchchv <ipchchv@gmail.com>
2022-07-16 14:02:57 +0300

    Implement a function to convert data into JSON format

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"encoding/json"
+	"log"
+)
+
+func getJSON(pre string, str string) []byte {
+	var res []byte
+	s, err := json.MarshalIndent(str, "\t", "\t")
+	if err != nil {
+		log.Panic(err)
+	}
+	if pre != "" {
+		pr, err := json.MarshalIndent(pre, "\t", "\t")
+		if err != nil {
+			log.Panic(err)
+		}
+		s = append(pr, s...)
+	}
+	res = append(res, s...)
+	return res
+}
+
 func main() {
 	server()
 }
