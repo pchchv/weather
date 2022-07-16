fe9906e929a539a399de3463b7605bb78c35b790
pchchv <ipchchv@gmail.com>
2022-07-16 14:09:06 +0300

    Add ping method tests

---
+++ main_test.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+	"testing"
+	"time"
+
+	vegeta "github.com/tsenart/vegeta/v12/lib"
+)
+
+func TestServerPing(t *testing.T) {
+	res, err := http.Get("http://127.0.0.1:8080/ping")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("status not OK")
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			t.Error(err)
+		}
+	}(res.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	b := string(body)
+	if !strings.Contains(b, "Weather Service") {
+		t.Fatal()
+	}
+}
+
+func TestLoadPing(t *testing.T) {
+	rate := vegeta.Rate{Freq: 1000, Per: time.Second}
+	duration := 5 * time.Second
+	targeter := vegeta.NewStaticTargeter(vegeta.Target{
+		Method: "GET",
+		URL:    "http://localhost:8080/ping",
+	})
+	attacker := vegeta.NewAttacker()
+	var metrics vegeta.Metrics
+	for res := range attacker.Attack(targeter, rate, duration, "Big Bang!") {
+		metrics.Add(res)
+	}
+	metrics.Close()
+	log.Printf("99th percentile: %s\n", metrics.Latencies.P99)
+}
--- server.go
+++ server.go
@@ -7,7 +7,7 @@ import (
 
 func ping(w http.ResponseWriter, _ *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	_, err := w.Write(getJSON("", "Random Value Service. Version 0.2"))
+	_, err := w.Write(getJSON("", "Weather Service. Version 0.0.1"))
 	if err != nil {
 		log.Panic(err)
 	}
