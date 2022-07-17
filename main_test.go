189a957bd1d85326110470f74f276483b3a4e9f9
pchchv <ipchchv@gmail.com>
2022-07-17 16:16:06 +0300

    Add stats method tests

--- main_test.go
+++ main_test.go
@@ -140,3 +140,46 @@ func TestLoadTime(t *testing.T) {
 	metrics.Close()
 	log.Printf("99th percentile: %s\n", metrics.Latencies.P99)
 }
+
+func TestServerStats(t *testing.T) {
+	res, err := http.Get("http://127.0.0.1:8080/stats?city=Paris")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("status not OK")
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			t.Error(err)
+		}
+	}(res.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	b := string(body)
+	if !strings.Contains(b, "Weather Service") {
+		t.Fatal()
+	}
+}
+
+func TestLoadStats(t *testing.T) {
+	rate := vegeta.Rate{Freq: 1000, Per: time.Second}
+	duration := 5 * time.Second
+	targeter := vegeta.NewStaticTargeter(vegeta.Target{
+		Method: "GET",
+		URL:    "http://localhost:8080/stats?city=Lviv",
+	})
+	attacker := vegeta.NewAttacker()
+	var metrics vegeta.Metrics
+	for res := range attacker.Attack(targeter, rate, duration, "Big Bang!") {
+		metrics.Add(res)
+	}
+	metrics.Close()
+	log.Printf("99th percentile: %s\n", metrics.Latencies.P99)
+}
